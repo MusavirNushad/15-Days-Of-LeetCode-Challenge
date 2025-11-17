28afefbff4a303e32419729e753e7f1332842c62
MusavirNushad <134808393+MusavirNushad@users.noreply.github.com>
2025-11-17 10:55:03 +0500

    Time: 7 ms (80.42%) | Memory: 21 MB (65.80%) - LeetSync

--- 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.py
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.py
@@ -2,17 +2,17 @@ class Solution:
     def kLengthApart(self, nums: List[int], k: int) -> bool:
         
         count = 0
-        first_one_seen = False   # to ignore the first 1
+        first_one_seen = False  
 
         for i in range(len(nums)):
             if nums[i] == 0:
                 count += 1
-            else:   # nums[i] == 1
-                if first_one_seen:               # for every 1 after the first one
-                    if count < k:                # check spacing
+            else:  
+                if first_one_seen:            
+                    if count < k:                
                         return False
-                first_one_seen = True            # mark first 1
-                count = 0                        # reset count after 1
+                first_one_seen = True          
+                count = 0                        
 
         return True
 
