57d464f21fd18882d7cf4d20d5392eb59bdaa35f
MusavirNushad <134808393+MusavirNushad@users.noreply.github.com>
2025-12-11 07:47:09 +0500

    Time: 14 ms (58.66%) | Memory: 34.8 MB (20.14%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,16 +1,11 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         
-        countDup = {}
+        d = dict()
 
         for num in nums:
-            if num in countDup:
-                countDup[num] += 1
-            else:
-                countDup[num] = 1
-        
-        for k in nums:
-            if countDup[k] > 1:
+            if num in d:
                 return True
-        
+            else:
+                d[num] = None
         return False
